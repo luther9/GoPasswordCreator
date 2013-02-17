e7543af762c2b2b1cfed20fbdece9017029c8194
Adam Jimerson <vendion@gmail.com>
2013-02-17 10:18:50 -0500

    Ran go fmt
    Cleaned up code with go fmt.
    
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 )
 
 var (
-        passwordLength = 8
+	passwordLength = 8
 
 	// Variables that define what characters to use in the password
 	lowerCase         bool
@@ -29,10 +29,10 @@ var (
 	specialCharacters bool
 	usersCharacters   string
 
-        // Make sure there will always be at least one password
-        passwordCount = 1
+	// Make sure there will always be at least one password
+	passwordCount = 1
 
-        file string
+	file string
 )
 
 func usage() {
@@ -65,78 +65,78 @@ func main() {
 		// Separate the subcommand from the value
 		parsed := strings.SplitN(arg, "=", 2)
 
-                // Group arguments by the data type of their value
-                switch parsed[0] {
-                case "own":
-                        // Need a string value
-                        if len(parsed) == 2 {
-                                usersCharacters = parsed[1]
-                        } else {
-                                printError(fmt.Errorf("'own' requires a '=' to specify characters"))
-                        }
-                case "-count":
-                        // Need a int value
-                        if len(parsed) == 2 {
-                                // Fetch the int value from parsed[1]
-                                var err error
-                                passwordCount, err = strconv.Atoi(parsed[1])
-                                if err != nil {
-                                        printError(err)
-                                }
-                        } else {
-                                printError(fmt.Errorf("'-count' requires a '=' to specify number of passwords"))
-                        }
-                case "-length":
-                        // Need a int value
-                        if len(parsed) == 2 {
-                                // Fetch the in value from parsed[1]
-                                var err error
-                                passwordLength, err = strconv.Atoi(parsed[1])
-                                if err != nil {
-                                        printError(err)
-                                }
-                        } else {
-                                printError(fmt.Errorf("'-length' requires a '=' to specify the length passwords should be"))
-                        }
-                case "-file":
-                        // Need a string value
-                        if len(parsed) == 2 {
-                                file = parsed[1]
-                        } else {
-                                printError(fmt.Errorf("'-file' requires a '=' to specify the file name to write passwords to"))
-                        }
-                default:
-                        // All other arguments take boolean values
-                        on := true
-                        if len(parsed) == 2 {
-                                var err error
-                                on, err = strconv.ParseBool(parsed[1])
-                                if err != nil {
-                                        printError(err)
-                                }
-                        }
-                        switch parsed[0] {
-                        case "all":
-                                lowerCase = on
-                                upperCase = on
-                                numerals = on
-                                specialCharacters = on
-                        case "alphanum":
-                                lowerCase = on
-                                upperCase = on
-                                numerals = on
-                        case "lower":
-                                lowerCase = on
-                        case "upper":
-                                upperCase = on
-                        case "numbers":
-                                numerals = on
-                        case "special":
-                                specialCharacters = on
-                        default:
-                                printError(fmt.Errorf("Invalid argument: %s", parsed[0]))
-                        }
-                }
+		// Group arguments by the data type of their value
+		switch parsed[0] {
+		case "own":
+			// Need a string value
+			if len(parsed) == 2 {
+				usersCharacters = parsed[1]
+			} else {
+				printError(fmt.Errorf("'own' requires a '=' to specify characters"))
+			}
+		case "-count":
+			// Need a int value
+			if len(parsed) == 2 {
+				// Fetch the int value from parsed[1]
+				var err error
+				passwordCount, err = strconv.Atoi(parsed[1])
+				if err != nil {
+					printError(err)
+				}
+			} else {
+				printError(fmt.Errorf("'-count' requires a '=' to specify number of passwords"))
+			}
+		case "-length":
+			// Need a int value
+			if len(parsed) == 2 {
+				// Fetch the in value from parsed[1]
+				var err error
+				passwordLength, err = strconv.Atoi(parsed[1])
+				if err != nil {
+					printError(err)
+				}
+			} else {
+				printError(fmt.Errorf("'-length' requires a '=' to specify the length passwords should be"))
+			}
+		case "-file":
+			// Need a string value
+			if len(parsed) == 2 {
+				file = parsed[1]
+			} else {
+				printError(fmt.Errorf("'-file' requires a '=' to specify the file name to write passwords to"))
+			}
+		default:
+			// All other arguments take boolean values
+			on := true
+			if len(parsed) == 2 {
+				var err error
+				on, err = strconv.ParseBool(parsed[1])
+				if err != nil {
+					printError(err)
+				}
+			}
+			switch parsed[0] {
+			case "all":
+				lowerCase = on
+				upperCase = on
+				numerals = on
+				specialCharacters = on
+			case "alphanum":
+				lowerCase = on
+				upperCase = on
+				numerals = on
+			case "lower":
+				lowerCase = on
+			case "upper":
+				upperCase = on
+			case "numbers":
+				numerals = on
+			case "special":
+				specialCharacters = on
+			default:
+				printError(fmt.Errorf("Invalid argument: %s", parsed[0]))
+			}
+		}
 
 	}
 
